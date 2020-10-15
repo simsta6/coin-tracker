55f73db31b4339453c095c5aba38ca1b5a7e9e53
simsta6 <51540204+simsta6@users.noreply.github.com>
2020-10-15 21:04:17 +0300

    Initial commit

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	r "github.com/simsta6/junior-task-go/internal"
+)
+
+func main() {
+	fmt.Println("hello world!")
+	rules, _ := readFromJSON("../rules.json")
+
+	for _, v := range rules {
+		fmt.Println(v)
+	}
+}
+
+func readFromJSON(filePath string) ([]r.Rule, error) {
+	var rules []r.Rule
+	jsonFile, err := os.Open(filePath)
+	defer jsonFile.Close()
+
+	if err != nil {
+		return rules, err
+	}
+
+	err = json.NewDecoder(jsonFile).Decode(&rules)
+
+	if err != nil {
+		return rules, err
+	}
+
+	return rules, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/simsta6/junior-task-go
+
+go 1.15
